9f1e3c6f29e04f730766d2f757d60f98876a8f15
Daniel Mihai <danielmihai@Daniels-Laptop.local>
2024-06-20 18:29:07 +0300

    push fast

--- src/axiosInstance/axiosInstance.ts
+++ src/axiosInstance/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:8888'
-    : '';
+    : 'https://tutorialx-backend-58995b2e84cc.herokuapp.com';
 
 
 export const axiosInstance = axios.create({
